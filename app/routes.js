af2bf822ebd5f09f264f0544df56044fd102e34a
chenxuewei <xueweihero@163.com>
2016-07-17 13:56:03 +0800

    change router

--- app/routes.js
+++ app/routes.js
@@ -1,54 +1,45 @@
 var Todo = require('./models/todo');
 
-module.exports = function (app){
-
-  // api ------------------------------------------
-  // get all todos
-  app.get('/api/todos', function(req, res){
-    Todo.find(function(err, todos){
-      if(err)
-        res.send(err);
-      res.json(todos);
-    });
-  });
-
-  // create todo and send back all todos after creation
-  app.post('/api/todos', function(req, res){
-
-    // create a todo, information comes from AJAX request from Angular
-     Todo.create({
-       text: req.body.text,
-       done: false
-     }, function(err, todo){
-       if(err)
-           res.send(err);
-
-       // get and return all the todos after you create another
-       Todo.find(function(err, todos){
-         if(err)
-             res.send(err)
-         res.json(todos);
-       });
-     });
-  });
-
-  app.delete('/api/tods/:todo_id', function(req, res){
-      Todo.remove({
-        _id: req.params.todo_id
-      }, function(err, todo){
-        if(err)
-           res.send(err);
-
-        // get and return all the todos after you create another
-        Todo.find(function(err, todos){
-          if(err)
+function getTodos(res) {
+    Todo.find(function (err, todos) {
+        if (err) {
             res.send(err);
-          res.json(todos);
-        });
-      });
-  });
+        }
+        res.json(todos);
+    });
+}
+;
 
-  app.get('*', function (req, res) {
-      res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-  });
+module.exports = function (app) {
+    // api ---------------------------------------------------------------------
+    app.get('/api/todos', function (req, res) {
+        getTodos(res);
+    });
+
+    app.post('/api/todos', function (req, res) {
+        Todo.create({
+            text: req.body.text,
+            done: false
+        }, function (err, todo) {
+            if (err)
+                res.send(err);
+            getTodos(res);
+        });
+    });
+
+    // delete a todo
+    app.delete('/api/todos/:todo_id', function (req, res) {
+        Todo.remove({
+            _id: req.params.todo_id
+        }, function (err, todo) {
+            if (err)
+                res.send(err);
+            getTodos(res);
+        });
+    });
+
+    // application -------------------------------------------------------------
+    app.get('*', function (req, res) {
+        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
 };
