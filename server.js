fb954fcc72af19fca31e9dd19a3f5230b7837bab
chenxuewei <xueweihero@163.com>
2016-07-18 14:25:54 +0800

    add database path in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express     = require('express');
 var app         = express();
 var mongoose    = require('mongoose');
 var morgan      = require('morgan');
+var database = require('./config/database'); 
 var bodyParaser = require('body-parser');
 var methodOverride = require('method-override');
 
